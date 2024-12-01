574bcdb66206d47293fa518b3f52e9f7368e977e
Ricardo Lujan <riickyluja21@gmail.com>
2024-12-01 20:33:25 -0300

    feat : add base plugin

---
+++ packages/better-auth-nile/call.ts
@@ -0,0 +1,38 @@
+import { type Context } from "better-call";
+import type { Session, User } from "../../db/schema";
+import { createAuthMiddleware } from "../../api/call";
+import { sessionMiddleware } from "../../api";
+import type { Role, defaultRoles } from "./access";
+import type { OrganizationOptions } from "./organization";
+
+export const orgMiddleware = createAuthMiddleware(async (ctx) => {
+	return {} as {
+		orgOptions: OrganizationOptions;
+		roles: typeof defaultRoles & {
+			[key: string]: Role<{}>;
+		};
+		getSession: (context: Context<any, any>) => Promise<{
+			session: Session & {
+				activeOrganizationId?: string;
+			};
+			user: User;
+		}>;
+	};
+});
+
+export const orgSessionMiddleware = createAuthMiddleware(
+	{
+		use: [sessionMiddleware],
+	},
+	async (ctx) => {
+		const session = ctx.context.session as {
+			session: Session & {
+				activeOrganizationId?: string;
+			};
+			user: User;
+		};
+		return {
+			session,
+		};
+	},
+);
